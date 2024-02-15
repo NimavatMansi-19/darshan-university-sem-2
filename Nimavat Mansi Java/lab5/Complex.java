f0ebc62223c91cb710805174a1bd905dc3d6a064
NimavatMansi-19 <144536348+NimavatMansi-19@users.noreply.github.com>
2024-02-15 13:37:32 +0530

    Add files via upload

---
+++ Nimavat Mansi Java/lab5/Complex.java
@@ -0,0 +1,20 @@
+class Com{
+	int real;
+	int imaginary;
+	Com(int a,int b){
+		a=0;
+		b=0;
+	}
+	Com(int a){
+		a=0;
+
+	}
+	
+}
+public class Complex{
+	public static void main(String[] args) {
+		Com c1=new Com(3,4);
+		Com c2=new Com(4,5);
+		
+	}
+}
