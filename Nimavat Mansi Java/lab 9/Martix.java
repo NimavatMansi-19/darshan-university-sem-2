27edf5658c623b6feebe1b5b9412a671687a662f
NimavatMansi-19 <144536348+NimavatMansi-19@users.noreply.github.com>
2024-03-21 13:40:19 +0530

    Add files via upload

---
+++ Nimavat Mansi Java/lab 9/Martix.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class A extends Thread{
+	int i=0;
+	int[][] a;
+
+	A(i,a){}
+
+	public void run(){
+		for(int j=0;j<a[0].legth;j++){
+			sum+=a[i][j];
+		}
+	}
+}
+
+
+public class Matrix{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+
+		for(int i=0;i<a.length;i++) i,a
+	}
+}
