f63fdc3842946a3b7e7f0229132f34ef298267ad
NimavatMansi-19 <144536348+NimavatMansi-19@users.noreply.github.com>
2024-03-26 12:43:39 +0530

    Add files via upload

--- Nimavat Mansi Java/Practice/meth2.java
+++ Nimavat Mansi Java/Practice/meth2.java
@@ -1,34 +1,45 @@
 import java.util.*;
 import java.text.DecimalFormat;
+import java.text.NumberFormat;
 public class meth2{
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		DecimalFormat df = new DecimalFormat("#.####");
+		// NumberFormat nf = NumberFormat.getInstance();
+  //  		nf.setMinimumFractionDigits(4);
 		System.out.println("How many elemants you want to Access:");
 		int a=sc.nextInt();
 		System.out.println("At which value you have to find Answer:");
 		double xn=sc.nextDouble();
-		int[] x=new int[a];
-		int[] y=new int[a];
-		int[] z=new int[a];
+		double[] x=new double[a];
+		double[] y=new double[a];
+		double[] z=new double[a];
 		z=y;
 		for(int i=0;i<x.length;i++){
 			System.out.println("Enter value of X"+i);
-			 x[i]=sc.nextInt();
+			 x[i]=sc.nextDouble();
+			 x[i]=Double.valueOf(df.format(x[i]));
+
+
 		}
 		//System.out.println(x[0]);
 		for(int i=0;i<y.length;i++){
 			System.out.println("Enter value of Y"+i);
-			 y[i]=sc.nextInt();
+			 y[i]=sc.nextDouble();
+			 y[i]=Double.valueOf(df.format(y[i]));
+			
+			 
 		}
-		int[] element=new int[a];
+		double[] element=new double[a];
 		//element[0]=y[0];
 		for(int i=a;i>=1;i--){
-			int[] del=new int[i-1];
+			double[] del=new double[i-1];
 			element[a-i]=y[0];
 			for(int j=0;j<y.length-1;j++){
 				del[j]=y[j+1]-y[j];
-				//System.out.println(del[j]);
+				del[j]=Double.valueOf(df.format(del[j]));
+                //System.out.println(del[j]);
+	
 			}
 				y=del;
 		
@@ -40,9 +51,10 @@ public class meth2{
 		System.out.println(p);
 		//function m=new function();
 		for(int i=0;i<a-1;i++){
-			element[i]=Integer.valueOf(df.format(element[i]));
+			element[i]=Double.valueOf(df.format(element[i]));
 			ans=ans+((func(i,temp)*element[i+1])/fac(i+1)) ;
-			
+			ans=Double.valueOf(df.format(ans));
+
 		}
 		System.out.println(ans);
 		//System.out.println(fac(3));
@@ -71,6 +83,7 @@ public static double func(int a,double p){
 	return value;
 	
 	
+
 
 }
 }
--- Nimavat Mansi Java/Practice/method2.java
+++ Nimavat Mansi Java/Practice/method2.java
@@ -1,57 +1,70 @@
-import java.util.*;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+
+    
+    //how to fix ans upto 4 digit.
+//recursion
 public class method2{
 	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
-		System.out.println("How much value you want to Enter:");
-		int m=sc.nextInt();
-		int j=3;
-		int[] x=new int[m];
-		int[] y=new int[m];
-		int[][] dely=new int[m-1][j];
-		System.out.println("Enter the value where you need to find Answer:" );
-		int a=sc.nextInt();
-		//System.out.println(a);
-		double p;
-		double ans;
-		for(int i=0;i<x.length;i++){
-			System.out.println("Enter value of X"+i);
-			 x[i]=sc.nextInt();
-		}
-		//System.out.println(x[0]);
-		for(int i=0;i<y.length;i++){
-			System.out.println("Enter value of Y"+i);
-			 y[i]=sc.nextInt();
-		}
+		// Scanner sc= new Scanner(System.in);
+		// DecimalFormat df = new DecimalFormat("#.####");
+		// System.out.println("How much value you want to Enter:");
+		// int m=sc.nextInt();
+		// int j=3;
+		// int[] x=new int[4];
+		// float[] y=new float[4];
+		// float[][] dely=new float[3][j];
+		// System.out.println("Enter the value where you need to find Answer:" );
+		// int a=sc.nextInt();
+		// //System.out.println(a);
+		// float p;
+		// float ans;
+		// for(int i=0;i<x.length;i++){
+		// 	System.out.println("Enter value of X"+i);
+		// 	 x[i]=sc.nextInt();
+		// }
+		// //System.out.println(x[0]);
+		// for(int i=0;i<y.length;i++){
+		// 	System.out.println("Enter value of Y"+i);
+		// 	 y[i]=sc.nextFloat();
+		// }
 		
 			
-			p=(a-x[0])/2.0;
-		System.out.println("Value of p is:"+p);
-		int n=0;
-		for(int i=0;i<m-1;i++){
-			for(int k=0;k<j;k++){
-				if(i==n){
-					j=j-i;
-				dely[n][k]=y[k+1]-y[k];
-				System.out.println("y0:"+dely[n][k]);
-			}
-				/*if(i==n){
-					j=j-i;
-				dely[1][k]=dely[0][k+1]-dely[0][k];
-				System.out.println("y1:"+dely[1][k]);	
-				}
-				if(i==n){
-				j=j-i;
-				dely[2][k]=dely[1][k+1]-dely[1][k];
-				System.out.println("y2:"+dely[2][k]);
-				}*/
-				if(i==m-1){
-					break;
-				}
+		// 	p=(a-x[0])/2f;
+		// System.out.println("Value of p is:"+p);
+		// for(int i=0;i<3;i++){
+		// 	for(int k=0;k<j;k++){
+		// 		if(i==0){
+		// 		j=j-i;
+		// 		dely[0][k]=y[k+1]-y[k];
+		// 		dely[0][k]=Float.valueOf(df.format(dely[0][k]));
+		// 		System.out.println("y0:"+dely[0][k]);
+		// 	}
+		// 		if(i==1){
+		// 			j=j-i;
+		// 		dely[1][k]=dely[0][k+1]-dely[0][k];
+		// 		dely[1][k]=Float.valueOf(df.format(dely[1][k]));
+		// 		System.out.println("y1:"+dely[1][k]);	
+		// 		}
+		// 		if(i==2){
+		// 		j=j-i;
+		// 		dely[2][k]=dely[1][k+1]-dely[1][k];
+		// 		dely[2][k]=Float.valueOf(df.format(dely[2][k]));
+		// 		System.out.println("y2:"+dely[2][k]);
+		// 		}
 
-		}
-		n++;
-		}
-		ans=y[0]+p*dely[0][0]+(p*(p-1.0)*dely[1][0])/2+(p*(p-1)*(p-2)*dely[2][0])/6;
-		System.out.println(ans);
+		// }
+		// }
+		// ans=y[0]+p*dely[0][0]+(p*(p-1f)*dely[1][0])/2f+(p*(p-1f)*(p-2f)*dely[2][0])/6f;
+		// System.out.println(ans);
+		double operation = -7.0e^-4;
+            BigDecimal big = new BigDecimal(operation);     
+            big = big.setScale(4, RoundingMode.HALF_UP);        
+            double d2 = big.doubleValue();
+            System.out.println(String.format("operation : %s", operation));
+            System.out.println(String.format("scaled : %s", d2));
+ 
+		    
 	}
 }
