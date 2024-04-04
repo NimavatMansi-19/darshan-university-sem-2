193f4309cb2f7ca9344e537ad5786dc82fd62c52
NimavatMansi-19 <144536348+NimavatMansi-19@users.noreply.github.com>
2024-04-04 05:40:25 +0530

    Add files via upload

---
+++ Nimavat Mansi Java/lab 10/FileInAndOut.java
@@ -0,0 +1,8 @@
+//Create a class called Student.
+// Write a student manager program to manipulate the student information 
+//from files by using FileInputStream and FileOutputStream.
+public class FileInAndOut{
+	public static void main(String[] args) {
+		
+	}
+}
